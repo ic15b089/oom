772d31a356292a21feea592163c4fe878aa58576
ic15b089 <mohammadtp66>
2016-05-18 16:50:17 +0200

    add project Task3

---
+++ tasks/Task3/Task3/Getraenk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Getraenk : Ware
+    {
+        private string m_alkohol;
+
+        public string Alkohol
+        {
+            get { return m_alkohol; }
+            set { m_alkohol = value; }
+        }
+        //-------------------------------
+
+        public Getraenk(int id, string name, double preis, string alkohol) : base(id, name, preis)
+        {
+            m_alkohol = alkohol;
+        }
+        //-------------------------------
+        public Getraenk() : this(-1, "", 0, "")
+        {
+
+        }
+        //------------------------------
+
+        public override void Write()
+        {
+            base.Write();
+            Console.WriteLine("Alkohol: {0}", Alkohol);
+        }
+    }
+}
---
+++ tasks/Task3/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ware a = new Ware(1, "Gulash", 10.15);
+            a.Write();
+            Console.WriteLine("------------------------------------");
+
+            Ware b = new Ware(2, "Orangesaft", 2.55);
+            b.Write();
+            Console.WriteLine("------------------------------------");
+
+            Gericht c = new Gericht(3, "Gulash", 10.15, "Gemuese, Fleisch, Tomate");
+            c.Write();
+            Console.WriteLine("------------------------------------");
+
+            Getraenk d = new Getraenk(4, "Gulash", 2.55, "Frei");
+            d.Write();
+            Console.WriteLine("------------------------------------");
+
+        }
+    }
+}
